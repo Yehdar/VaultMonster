281d124fc03128ca278f02bf4ced0d2313009850
Yehdar <p.radhey06@gmail.com>
2023-11-19 13:55:58 -0500

    test: check API

--- server/main.go
+++ server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import ("fmt")
+import (
+  "fmt"
+  "log"
+  "github.com/gofiber/fiber/v2"
+)
 
 func main() {
   fmt.Print("hollup... let him cook")
+
+  app := fiber.New()
+
+  app.Get("/healthcheck", func(c *fiber.Ctx) error {
+    return c.SendString("OK bruh")
+  })
+
+  log.Fatal(app.Listen(":4000"))
 }

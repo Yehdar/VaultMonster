a29d294169ba3eab2e9882fc87b26a33ef57f7f5
Yehdar <p.radhey06@gmail.com>
2023-11-20 12:46:11 -0500

    feat: fetch API to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import useSWR from "swr";
+
+export const ENDPOINT = "http://localhost:4000";
+
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
+  const { data, mutate } = useSWR("api/todos", fetcher);
+
   return (
     <>
-      <h1>hollup... let him cook</h1>
+      <h1>{JSON.stringify(data)}</h1>
     </>
   );
 }
